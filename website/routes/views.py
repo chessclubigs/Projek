c04c3e4bd7851aab19c058ba808e7964acd4f3f4
ezrazka <ezra.a.einnos@gmail.com>
2023-12-22 21:36:11 +0700

    added basic css page layout.

--- website/routes/views.py
+++ website/routes/views.py
@@ -4,4 +4,12 @@ views = Blueprint('views', __name__)
 
 @views.route("/")
 def index():
-    return render_template("base.html")
+    return render_template("index.html")
+
+@views.route("/leaderboard")
+def leaderboard():
+    return render_template("leaderboard.html", title="Leaderboard")
+
+@views.route("/tournaments")
+def tournaments():
+    return render_template("tournaments.html", title="Tournaments")
