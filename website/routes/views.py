bba16492c58fd413ee444a7ab839fbaeee166af2
ezrazka <ezra.a.einnos@gmail.com>
2024-02-17 15:34:26 +0700

    added leaderboard pagination.

--- website/routes/views.py
+++ website/routes/views.py
@@ -25,21 +25,21 @@ def leaderboard_top_rated():
     page = request.args.get("page", 1, type=int)
     members = Member.query.order_by(Member.rating.desc())\
         .paginate(page=page, per_page=10)
-    return render_template("leaderboard-top-rated.html", title="Leaderboard", members=members)
+    return render_template("leaderboard-top-rated.html", title="Leaderboard", members=members, page=page)
 
 @views.route("/leaderboard/win-rate")
 def leaderboard_win_rate():
     page = request.args.get("page", 1, type=int)
     members = Member.query.order_by(Member.rating.desc())\
         .paginate(page=page, per_page=10)
-    return render_template("leaderboard-win-rate.html", title="Leaderboard", members=members)
+    return render_template("leaderboard-win-rate.html", title="Leaderboard", members=members, page=page)
 
 @views.route("/leaderboard/tournaments-participated")
 def leaderboard_tournaments_participated():
     page = request.args.get("page", 1, type=int)
     members = Member.query.order_by(Member.rating.desc())\
         .paginate(page=page, per_page=10)
-    return render_template("leaderboard-tournaments-participated.html", title="Leaderboard", members=members)
+    return render_template("leaderboard-tournaments-participated.html", title="Leaderboard", members=members, page=page)
 
 @views.route("/tournaments")
 def tournaments():
